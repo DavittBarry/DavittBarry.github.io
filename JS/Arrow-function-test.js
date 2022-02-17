9afce6ebc5fb8a1d237c59030a2e4e2b06b4d6db
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2022-02-17 09:44:41 +0200

    Update Arrow-function-test.js

--- JS/Arrow-function-test.js
+++ JS/Arrow-function-test.js
@@ -1,14 +1,16 @@
+// Luodaan nuolifunktio. Se käyttää parametrina 'nimi'.
+
 let tervehdys = ( nimi ) => {
   alert(`Moikka ${nimi}`);
 }
 
-/**function tervehdys(nimi) => {
-  alert('Moikka ${nimi}');
-} **/
+// Tehdään funktio joka kysyy käyttäjältä nimensa.
 
 function syotteenLukeminen(takaisinkutsu) {
     let nimi = prompt('Kerro nimesi');
     takaisinkutsu(nimi);
 }
+
+/ Tulostetaan sen nimi ja tervehdys.
 
 syotteenLukeminen(tervehdys);
